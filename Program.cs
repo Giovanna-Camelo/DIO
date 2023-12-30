1c7a1428fe7311556eecb4c7c34a01ac7cb91e46
Giovanna Camelo <gigicamelo17@gmail.com>
2023-12-30 18:20:12 -0300

    Desafio Concluido com Suceso

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia = new Nokia(numero:"123456", modelo:"Nokia G60", imei:"2222222222222", memoria:128);
+
+
+nokia.Ligar();
+nokia.InstalarAplicativo("Instagram");
+
+Console.WriteLine("\n");
+
+
+Console.WriteLine("Smartphone iphone: ");
+Smartphone iphone = new Iphone(numero:"789102", modelo:"iphone 15", imei:"333333333333", memoria:512);
+
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Spotify");
